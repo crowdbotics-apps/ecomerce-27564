6e7b5399e16feac2409f08f12fa6689b78e098ad
Crowdbotics <team@crowdbotics.com>
2021-05-29 17:34:57 +0000

    Update models

--- ecomerce_27564/wsgi.py
+++ ecomerce_27564/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomerce_27564.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecomerce_27564.settings")
 
 application = get_wsgi_application()
